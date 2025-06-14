5bdca9dca09165f2bbb3267bebc6107bac71a60d
Eusebio <eusebio_olvera9494@hotmail.com>
2025-06-13 18:32:25 -0600

    Se añade otra forma de realizar el cálculo

--- src/services/getInvoiceServices.js
+++ src/services/getInvoiceServices.js
@@ -3,10 +3,14 @@ import { invoice } from "../data/invoice"
 export const getInvoice = () => {
     
     // console.log(invoice)
-    let total = 0;
-    invoice.items.forEach( item => {
-        total += item.price * item.quantity;
-    });
+    // let total = 0;
+    // invoice.items.forEach( item => {
+    //     total += item.price * item.quantity;
+    // });
+
+    const total = invoice.items
+    .map( item => item.price * item.quantity)
+    .reduce( (accumulator, currentValue) => accumulator + currentValue, 0);//empieza en 0 el acumulador
 
     return {...invoice, total};
 }
